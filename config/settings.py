3accdd56e56b79203cf4924862dc6037502bba4d
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2025-04-28 09:50:45 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,3 @@
-# config/settings.py
-
 SECRET_KEY = "your_flask_secret_key_here"
 ENCRYPTION_KEY = b'your_32_byte_aes_key_here___'  # Must be 32 bytes for AES-256
 TWO_FACTOR_SECRET = "your_totp_seed_here"
